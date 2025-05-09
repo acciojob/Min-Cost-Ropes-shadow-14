251b57ea7af2569f7ebd35501930b0fd7e47712f
Sarthak Goyal <82680055+shadow-14@users.noreply.github.com>
2025-05-09 17:39:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function mincost(arr)
 { 
 //write your code here
+	
 // return the min cost
-  
+	arr.sort(function (a,b){return a-b})
+	let sum =0;
+	let ans = arr.forEach((element,i)=>{sum+=element});
+  return ans;
 }
 
 module.exports=mincost;
